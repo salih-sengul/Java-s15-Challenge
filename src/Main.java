8a8a51d7d6442ff3300c2dc4c4314c24aeb1bfa4
salih-sengul <mustafabas19051994@gmail.com>
2025-12-18 23:41:27 +0300

    Recent changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,155 @@
+import com.workintech.library.Library;
+import com.workintech.user.Admin;
+import com.workintech.user.Athentication;
+import com.workintech.user.Librarian;
+import com.workintech.user.User;
+
+import java.util.*;
+
+import static java.lang.System.out;
+
 public class Main {
+    static Scanner scan = new Scanner(System.in);
+    static String sessionName;
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        String keyboard;
+        Admin admin = new Admin(1L,"ali","123");
+        Library.getUsers().put(admin.getId(),admin);
+        String session="";
+
+        String auth;
+        while(true){
+            System.out.println("kullanıcı Adı girin:");
+            String username = scan.nextLine();
+            sessionName =username;
+
+            System.out.println("Şifre girin");
+            String password = scan.nextLine();
+
+            Athentication athentication = new Athentication(username,password);
+            auth = athentication.checkNamePassword(username,password);
+
+            System.out.println(auth);
+
+            for(Map.Entry<Long,User> entry: Library.getUsers().entrySet()){
+                if(entry.getValue().getUserName().equals(username)){
+                    session =(entry.getValue() instanceof Admin)?("Admin"):( "Librarian");
+                }
+            }
+
+            if(auth.equals("user logged in")){
+                break;
+            }
+
+        }
+
+        System.out.println(session);
+
+        if(session.equals("Admin")){
+            System.out.println("Admin metodlarına girmek istiyor musunuz? evet is (devam) yazın.");
+            if(scan.nextLine().trim().equals("devam")){
+                adminHelpMenu();
+                while (!(keyboard = scan.nextLine()).equals("exit")){
+                    switch(keyboard){
+                        case "createAdmin"-> createAdmin();
+                        case "deleteAdmin" -> deleteAdmin();
+                        case "createLib" -> createLibrarian();
+                        case "deleteLib" -> deleteLibrarian();
+                        case "list" -> Library.listUsers();
+                        case "help" -> adminHelpMenu();
+                        default -> out.println("Bilinmeyen komut. help yazıp yardımcı menüyü açın");
+                    }
+                }
+            }
+
+        }
+
+        /*while(!(keyboard = scan.nextLine().toLowerCase(Locale.ROOT)).equals("exit")){
+
+            switch (keyboard){
+                case
+            }
+
+        }*/
     }
+
+    public static void adminHelpMenu(){
+            String help = """
+                - createAdmin : Admin kullanıcısı ekle
+                - deleteAdmin : Admin kullanıcısı sil
+                - createLib : Kütüphaneci kullanıcısı ekle
+                - deleteLib : Kütüphaneci kullanıcısı sil
+                - list : kullanıcıları listele
+                - exit : Admin komutlarından çık.
+                """;
+            out.println(help);
+
+    }
+
+    public static void createAdmin(){
+        System.out.println("Kullanıcı adı giriniz.");
+        String username = scan.nextLine().trim();
+
+        System.out.println("Şifre giriniz");
+        String password = scan.nextLine().trim();
+
+        Admin admin = new Admin(Library.getId(Library.getUsers()),username,password);
+        out.println(admin);
+        out.println(admin.createAdmin(admin));
+    }
+
+    public static void deleteAdmin(){
+        System.out.println("Silmek istediğiniz admin kullanıcı adı girin: ");
+        String adminName = scan.nextLine().trim();
+        if(sessionName.equals(adminName)){
+            out.println("Kendinizi silemezsiniz :D");
+            return;
+        }
+        for(Map.Entry<Long,User> entry: Library.getUsers().entrySet()){
+            if(entry.getValue() instanceof Admin ){
+                if(entry.getValue().getUserName().equals(adminName)){
+                    Library.getUsers().remove(entry.getValue().getId());
+                    System.out.println(adminName+" Kullanıcı adlı admin silindi.");
+                    return;
+                }
+            }
+        }
+        System.out.println("Map'te Admin kullanıcı yok");
+        System.out.println(adminName+" kullanıcı adlı kullanıcı bulunamadı!");
+    }
+
+    public static  void createLibrarian(){
+        System.out.println("Kütüphaneci kullanıcı adı girin: ");
+        String librarianName = scan.nextLine().trim();
+
+        System.out.println("Kütüphaneci şifre girin: ");
+        String librarianPass = scan.nextLine().trim();
+
+        Librarian librarian = new Librarian(Library.getId(Library.getUsers()),librarianName,librarianPass);
+        out.println(librarian);
+        Library.getUsers().put(librarian.getId(),librarian);
+    }
+
+    public  static void deleteLibrarian(){
+
+        System.out.println("Silmek istediğiniz kütüphaneci kullanıcı adı girin: ");
+        String librarianName = scan.nextLine().trim();
+
+        for(Map.Entry<Long,User> entry: Library.getUsers().entrySet()){
+            if(entry.getValue() instanceof Librarian ){
+                if(entry.getValue().getUserName().equals(librarianName)){
+                    Library.getUsers().remove(entry.getValue().getId());
+                    System.out.println(librarianName+" Kullanıcı adlı kütüphaneci silindi.");
+                    return;
+                }
+            }
+        }
+        System.out.println(librarianName+" kullanıcı adlı kullanıcı bulunamadı!");
+        System.out.println("Map'te kütüphaneci kullanıcı yok");
+    }
+
 }
---
+++ src/com/workintech/library/Library.java
@@ -0,0 +1,168 @@
+package com.workintech.library;
+
+import com.workintech.book.Book;
+import com.workintech.book.category.Category;
+import com.workintech.reader.Reader;
+import com.workintech.user.Admin;
+import com.workintech.user.Librarian;
+import com.workintech.user.User;
+
+import java.util.*;
+
+public class Library {
+
+    private static Map<Long,Book> books = new HashMap<>();
+    private static Map<Long,Reader> readers = new HashMap<>();
+    private static Map<Long, User> users = new HashMap<>();
+    private static Map<Long, Invoice> invoices= new HashMap<>();
+    private static Map<Long, Category> categories= new HashMap<>();
+
+    private static Library instance = new Library();
+
+    private Library(){
+        Admin admin = new Admin(1L,"admin","123");
+        Librarian librarian = new Librarian(2L,"veli","123");
+        users.put(2L,librarian);
+        users.put(admin.getId(),admin);
+    }
+
+    public static Library getInstance(){
+        return instance;
+    }
+
+    public static Map<Long, Book> getBooks() {
+        return books;
+    }
+
+    public static Map<Long, Reader> getReaders() {
+        return readers;
+    }
+
+
+    public static Map<Long, User> getUsers() {
+
+        return users;
+    }
+
+    public static Map<Long, Invoice> getInvoices() {
+        return invoices;
+    }
+
+    public static Map<Long, Category> getCategories() {
+        return categories;
+    }
+
+
+    public static Book findBook(Long id){
+        return books.get(id);
+    }
+
+    public static Book findBook(String name){
+        for(Map.Entry<Long,Book> entry: books.entrySet()){
+            if(entry.getValue().getName().equals(name)){
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
+    public static Book findBookAuthor(String authorName){
+        for(Map.Entry<Long,Book> entry: books.entrySet()){
+            if(entry.getValue().getName().equals(authorName)){
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
+    public static String lendBook(Book book, Reader reader){
+
+        if(reader.getBooks().add(book) && book.getAvailable()){
+            book.setAvailable(false);
+
+           Invoice invoice = new Invoice (book.getId(),book.getId(), book.getPrice());
+           invoices.put(invoice.getId(),invoice);
+           book.setInvoice(invoice);
+           reader.getInvoices().add(invoice);
+
+            return "book added on the reader";
+        }else{
+            return "book already on the reader";
+        }
+    }
+
+    public static String returnBook(Book book, Reader reader){
+        if(reader.getBooks().remove(book)){
+            book.setAvailable(true);
+            book.getInvoice().setEndDate(new Date());
+            book.setInvoice(null);
+
+            return "book returned";
+        }else{
+            return "user dosent have this book";
+        }
+    }
+
+    /*Map<Long,Book>
+    Map<Long,Reader>
+    Map<Long, User>
+    Map<Long, Invoice>
+    Map<Long, Category>*/
+
+    public static long getId(Map<Long,?> map){
+         if(map.isEmpty()) return 1L;
+         return Collections.max(map.keySet())+1;
+    }
+
+    public static List<Book> listByCategory(Category category){
+       return category.getBooks().stream().toList();
+    }
+
+    public static List<Book> listByAuthor(String authorName){
+        List<Book> bookList = new ArrayList<>();
+
+        for(Map.Entry<Long,Book> entry: books.entrySet()){
+            if(entry.getValue().getAuthor().equals(authorName)){
+                bookList.add(entry.getValue());
+            }
+        }
+        return bookList;
+    }
+
+    public static String addBook(Book book){
+        if(books.put(getId(books),book)==null){
+            return "Book added successfully";
+        }else{
+            return "id is occupied";
+        }
+    }
+
+    public static String deleteBook(Book book){
+        if(books.containsKey(book.getId())){
+            books.remove(book.getId());
+            return "book removed!";
+        }else{
+            return "book was not in the system";
+        }
+    }
+
+    public static String updateBook(Book oldBook,Book newBook){
+        if(books.containsKey(oldBook.getId())){
+            books.put(oldBook.getId(),newBook);
+            return "book updated";
+        }else {
+            return "No books founde";
+        }
+    }
+
+    public static void listUsers(){
+        for(Map.Entry<Long, User> entry: users.entrySet()){
+            System.out.println("id: " + entry.getKey()+" | "+entry.getValue());
+
+        }
+    }
+
+
+
+
+}
